b7ff729cdba8fcad3681f7df6af9bf328a55a6d8
José María Sánchez Salas <josemaria.sanchezsalas@gmail.com>
2017-04-24 12:49:55 +0200

    Some mistakes fixed

--- experiments/blurring-faces.py
+++ experiments/blurring-faces.py
@@ -3,11 +3,13 @@
 # Code on Github repository: https://github.com/jmssalas/Artificial-Vision-Exercises-2017
 
 # Press 'Esc' key for exit
+# Press 'f' key for entry in oneFaceMode
+# Use mouse for select face in oneFaceMode
 
 # Execute: ./blurring-faces.py [-h] [-path PATH]
 # options:
 #   - [-h] for help
-#   - [-path PATH] for indicate the path of library: default it use: $HOME/miniconda3/share/OpenCV/haarcascades/'
+#   - [-path PATH] for indicate the path of library: default it uses: $HOME/miniconda3/share/OpenCV/haarcascades/' (adaboost)
 
 ##################################
 ## PROBLEM STATEMENT (IN SPANISH)
@@ -67,6 +69,12 @@ minMatches = 5							# Minimum matches between getters faces and selected face
 selectedFace = None                     # Selected faces to oneFacesMode
 sigma = 15                              # Blurring factor
 
+
+# Function which update minMatches from trackbar
+def update(v):
+ global minMatches
+ minMatches = v
+
 # Mouse event handler
 def mouseEventHandler(event, x, y, flags, param):
     global x0, y0, xf, yf, drawing, lButtonUp
@@ -98,7 +106,6 @@ cv.namedWindow(programName)
 # Set mouseCallback to mouseEventHandler
 cv.setMouseCallback(programName, mouseEventHandler)
 
-
 # Function which get current frame
 def getFrame(cap):
     # Read frame
@@ -128,14 +135,14 @@ def getBetterMatches(desc1, desc2):
 
     return good
 
-# Function which draw ROI selected above 'frame' param with
+# Function which draws ROI selected above 'frame' param with
 # first vertex 'p0' and second vertex 'p1' and color 'color' param
 def drawROI(frame, p0, pf, color):
     cv.rectangle(frame, p0, pf, color)
 
 
 
-# Function which detect faces in 'img' image with 'face_cascade'
+# Function which detects faces in 'img' image with 'face_cascade'
 def detectFaces(img):
     return face_cascade.detectMultiScale(img)
 
@@ -155,28 +162,36 @@ def blurringFaces(faces, img):
         doBlurring(x,y,w,h,img,sigma)
 
 
+# Function which searches the face which has got like descriptor 'descriptor'
+# in all image 'img' and if finds it, then blurring it.
 def blurringFace(descriptor, img):
 
+    # Detect all faces in 'img'
     faces = detectFaces(img=img)
 
+    # For each face
     for (x, y, w, h) in faces:
         faceImg = img[y:y + h, x:x + w]
 
-        # Get descriptor vector of 'frame'
+        # Get descriptor vector of 'faceImg'
         descFace = getDescriptorVector(img=faceImg)
 
         # Get better matches between 'descriptor' and 'descFace'
         matches = getBetterMatches(descriptor, descFace)
 
-        # Check number of matches. If it is bigger than 'minMatches', then show model in other window
+        # Check number of matches. If it is bigger than 'minMatches', then blurring face
         if len(matches) >= minMatches :
-            doBlurring(x,y,w,h,faceImg,sigma)
+            doBlurring(x,y,w,h,img,sigma)
 
 
 # One Face Mode -> Face selected with ROI
 def oneFaceMode(cap):
-    global lButtonUp, drawing
+    global lButtonUp, drawing, x0, xf, y0, yf
 
+    # Create Trackbar on main window
+    cv.createTrackbar('minMatches', programName, minMatches, 255, update)
+
+    # Descriptor of selected face
     faceDesc = None
 
     while True:
@@ -194,8 +209,11 @@ def oneFaceMode(cap):
             lButtonUp = False
             drawing = False
 
-            # Get descriptor vector of ROI
+            # Get descriptor vector of selected face
             faceDesc = getDescriptorVector(img=frame[y0:yf + 1, x0:xf + 1])
+
+            # Delete ROI selection
+            x0, y0, xf, yf = initialPosition, initialPosition, initialPosition, initialPosition
 
         if faceDesc is not None:
             blurringFace(descriptor=faceDesc, img=frame)
@@ -206,6 +224,8 @@ def oneFaceMode(cap):
         # Show frame
         cv.imshow(programName, frame)
 
+    # Delete window for delete trackbar
+    cv.destroyWindow(programName)
 
 # Main function.
 # Default -> dev = 0
@@ -220,13 +240,16 @@ def play(dev=0):
 
         if key == escKey: break
 
-        #if key == oneFaceKey:
-        #    oneFaceMode(cap=cap)
+        if key == oneFaceKey:
+            # Activate one face mode
+            oneFaceMode(cap=cap)
 
         # Get frame
         frame = getFrame(cap=cap)
 
+        # Detect all faces
         faces = detectFaces(img=frame)
+        # Blurring them
         blurringFaces(faces=faces, img=frame)
 
         # Show frame
